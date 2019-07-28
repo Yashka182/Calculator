3216257fef56c1c004d92b8e6dacf2d54c9c142c
Yashka182 <36624939+Yashka182@users.noreply.github.com>
2019-07-28 14:02:04 +0300

    Update Handler.java

--- Handler.java
+++ Handler.java
@@ -1,5 +1,6 @@
 package com.netcracker.gurev.calculator;
 import io.netty.buffer.ByteBuf;
+import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandlerAdapter;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
@@ -7,22 +8,31 @@ import io.netty.util.CharsetUtil;
 import io.netty.util.ReferenceCountUtil;
 
 
-public class Handler extends ChannelInboundHandlerAdapter {
+public class Handler extends ChannelInboundHandlerAdapter{
 
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
         try{
-            ByteBuf in = (ByteBuf) msg;
+
+            //ByteBuf in = (ByteBuf) msg;
+            //String input = in.toString(CharsetUtil.US_ASCII);
             Calculator deepThought = new Calculator();
-            float result = deepThought.countServer(in.toString(CharsetUtil.US_ASCII));
-            ctx.writeAndFlush(result);
-            System.out.println(result);
+            String result = deepThought.countServer((String)msg);
+            Channel channel = ctx.channel();
+
+            channel.write("Answer: " + result + "\n");
+            channel.flush();
+            //System.out.println(result);
         }finally {
             ReferenceCountUtil.release(msg);
         }
     }
 
-
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("Channel active");
+        super.channelActive(ctx);
+    }
 }
 
 
