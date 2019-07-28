3188d408b60b93030e05fc708b466c2297768c5d
Yashka182 <36624939+Yashka182@users.noreply.github.com>
2019-07-28 14:00:54 +0300

    Update ClientHandler.java

--- ClientHandler.java
+++ ClientHandler.java
@@ -5,16 +5,15 @@ import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.util.CharsetUtil;
+import io.netty.util.ReferenceCountUtil;
 
 public class ClientHandler extends ChannelInboundHandlerAdapter {
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-
-        ByteBuf m = (ByteBuf)msg;
         try {
-            System.out.println(m.toString(CharsetUtil.US_ASCII));
+            System.out.println((String)msg);
         }finally {
-            m.release();
+            ReferenceCountUtil.release(msg);
         }
     }
 
